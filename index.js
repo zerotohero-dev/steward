ca72d55ab1e2a936e791f76870620c3f7de7e0cb
Volkan Ozcelik <volkan.ozcelik@jivesoftware.com>
2013-08-10 17:14:55 -0700

    implemented a basic loop logic.

--- index.js
+++ index.js
@@ -3,4 +3,32 @@
 var capture = require('./service/capture').snapshot;
 var inform = require('./service/mail').send;
 
+var config = require('./config').config;
+var kSnapshotCount = config.app.SNAPSHOT_COUNT;
+var kCaptureInterval = config.app.CAPTURE_INTERVAL;
+
 var counter = 0;
+var timerId = 0;
+
+function executeAfterSnapshot() {
+    if(counter > kSnapshotCount) {
+        counter = 0;
+
+        // compute diff sync
+
+        return;
+    }
+
+    loop();
+}
+
+
+function loop() {
+    clearTimeout(timerId);
+
+    timerId = setTimeout(function() {
+        capture(counter++, executeAfterSnapshot);
+    }, kCaptureInterval);
+}
+
+loop();
